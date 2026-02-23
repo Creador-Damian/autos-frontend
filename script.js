3cb985d4e3cbccf5c2f48bad07a17d339d643db9
Damian <a.damiansanz@gmail.com>
2026-02-23 15:56:45 -0300

    agregar boton editar frontend

--- script.js
+++ script.js
@@ -5,12 +5,35 @@ fetch("https://autos-backendss.onrender.com/autos")
 
         data.forEach(auto => {
             const autoHTML = `
-                <div class="auto">
-                    <h2>${auto.marca} ${auto.modelo}</h2>
-                    <p>Año: ${auto.año}</p>
-                    <p>Precio: $${auto.precio}</p>
-                </div>
-            `;
+  <div class="auto">
+    <h2>${auto.marca} ${auto.modelo}</h2>
+    <p>Año: ${auto.año}</p>
+    <p>Precio: $${auto.precio}</p>
+
+    <button onclick="editarAuto('${auto._id}')">Editar</button>
+    <button onclick="eliminarAuto('${auto._id}')">Eliminar</button>
+  </div>
+  
+`;
+function editarAuto(id) {
+  const nuevoPrecio = prompt("Ingrese nuevo precio:");
+
+  fetch(`https://autos-backendss.onrender.com/autos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      precio: Number(nuevoPrecio)
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    alert("Auto actualizado 🚗");
+    location.reload();
+  })
+  .catch(err => console.error(err));
+}
 
             contenedor.innerHTML += autoHTML;
         });
